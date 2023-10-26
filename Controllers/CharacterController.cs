fca8a09f94b5e359d4bb7bc7a82bfdcf6ab09728
Kirill Shugaev <kira.krut@yandex.ru>
2023-10-26 15:33:49 +0500

    Added dto's and automapper for them

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -18,19 +18,19 @@ namespace rpg.Controllers
         }
 
         [HttpGet("GetAll")]
-        public async Task<ActionResult<ServiceResponse<List<Character>>>> Get()
+        public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> Get()
         {
             return Ok(await _characterService.GetAllCharacters());
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<ServiceResponse<Character>>> GetSingleCharacter(int id)
+        public async Task<ActionResult<ServiceResponse<GetCharacterDto>>> GetSingleCharacter(int id)
         {
             return Ok(await _characterService.GetCharacterById(id));
         }
         
         [HttpPost]
-        public async Task<ActionResult<ServiceResponse<List<Character>>>> AddCharacter(Character newCharacter)
+        public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> AddCharacter(AddCharacterDto newCharacter)
         {
             return Ok(await _characterService.AddCharacter(newCharacter));
         }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
+global using AutoMapper;
 global using rpg.Models;
 global using rpg.Services.CharacterService;
+global using rpg.Dtos.Character;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +12,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+
 builder.Services.AddScoped<ICharacterService, CharacterService>();
 
 var app = builder.Build();
