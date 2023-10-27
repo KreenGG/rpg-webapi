f9ce014b2990e6baa1d76771d131e9e10cfddab2
Kirill Shugaev <kira.krut@yandex.ru>
2023-10-27 14:27:02 +0500

    Added Entity Framework and data context for database

---
+++ Data/DataContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace rpg.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Character> Characters => Set<Character>();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,17 @@
 global using AutoMapper;
+global using Microsoft.EntityFrameworkCore;
 global using rpg.Models;
 global using rpg.Services.CharacterService;
 global using rpg.Dtos.Character;
+global using rpg.Data;
+
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
